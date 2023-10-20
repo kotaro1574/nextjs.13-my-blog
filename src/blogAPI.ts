a427dd961bd1ab1c026fe128ecb1e83ea2cdf398
kotaro1574 <63211616+kotaro1574@users.noreply.github.com>
2023-10-20 13:53:59 +0000

    fix: getAllArticlesを修正

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -2,11 +2,10 @@ import { Article } from './types'
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch(
-    `https://silver-waddle-pwx9pxgwgvg296j4-3001.app.github.dev/posts`,
+    `http://0.0.0.0:3001/posts`,
     { cache: 'no-store' }, // SSR
   )
-  // console.log(res.json())
 
   const articles = await res.json()
-  return []
+  return articles
 }
