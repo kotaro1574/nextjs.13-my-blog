44aca15aeeb3bd7b22176d0adc4fef2ba9942c8d
kotaro1574 <63211616+kotaro1574@users.noreply.github.com>
2023-10-20 14:38:36 +0000

    update: articlesの取得＆表示

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,126 +1,51 @@
+import { Article } from '@/types'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const ArticleList = () => {
+type Props = {
+  articles: Article[]
+}
+
+const ArticleList = ({ articles }: Props) => {
   return (
     <div>
-      <article className={'flex flex-col shadow my-4'}>
-        <Link href={'#'} className={'hover:opacity-75'}>
-          <Image
-            src={
-              'https://source.unsplash.com/collection/1346951/1000x500?sig=1'
-            }
-            alt={''}
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className={'bg-white flex flex-col justify-start p-6'}>
-          <Link
-            href={'#'}
-            className={'text-blue-700 text-sm font-bold uppercase pb-4'}
-          >
-            Technology
+      {articles.map((article, index) => (
+        <article key={article.id} className={'flex flex-col shadow my-4'}>
+          <Link href={`articles/${article.id}`} className={'hover:opacity-75'}>
+            <Image
+              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${index}`}
+              alt={''}
+              width={1280}
+              height={300}
+            />
           </Link>
-          <Link
-            href={'#'}
-            className={
-              'text-3xl text-slate-900 font-bold hover:text-gray-700 pb-4'
-            }
-          >
-            Next.jsの勉強中
-          </Link>
-          <p className={'text-sm pb-3 text-slate-900'}>
-            Published on 2023/10/18
-          </p>
-          <Link href={'#'} className={'text-slate-900 pb-6'}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis
-            iaculis dui porta volutpat. In sit amet posuere magna..
-          </Link>
-          <Link href={'#'} className={'text-pink-800 hover:text-black'}>
-            続きを読む
-          </Link>
-        </div>
-      </article>
-      <article className={'flex flex-col shadow my-4'}>
-        <Link href={'#'} className={'hover:opacity-75'}>
-          <Image
-            src={
-              'https://source.unsplash.com/collection/1346951/1000x500?sig=2'
-            }
-            alt={''}
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className={'bg-white flex flex-col justify-start p-6'}>
-          <Link
-            href={'#'}
-            className={'text-blue-700 text-sm font-bold uppercase pb-4'}
-          >
-            Technology
-          </Link>
-          <Link
-            href={'#'}
-            className={
-              'text-3xl text-slate-900 font-bold hover:text-gray-700 pb-4'
-            }
-          >
-            Next.jsの勉強中
-          </Link>
-          <p className={'text-sm pb-3 text-slate-900'}>
-            Published on 2023/10/18
-          </p>
-          <Link href={'#'} className={'text-slate-900 pb-6'}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis
-            iaculis dui porta volutpat. In sit amet posuere magna..
-          </Link>
-          <Link href={'#'} className={'text-pink-800 hover:text-black'}>
-            続きを読む
-          </Link>
-        </div>
-      </article>
-      <article className={'flex flex-col shadow my-4'}>
-        <Link href={'#'} className={'hover:opacity-75'}>
-          <Image
-            src={
-              'https://source.unsplash.com/collection/1346951/1000x500?sig=3'
-            }
-            alt={''}
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className={'bg-white flex flex-col justify-start p-6'}>
-          <Link
-            href={'#'}
-            className={'text-blue-700 text-sm font-bold uppercase pb-4'}
-          >
-            Technology
-          </Link>
-          <Link
-            href={'#'}
-            className={
-              'text-3xl text-slate-900 font-bold hover:text-gray-700 pb-4'
-            }
-          >
-            Next.jsの勉強中
-          </Link>
-          <p className={'text-sm pb-3 text-slate-900'}>
-            Published on 2023/10/18
-          </p>
-          <Link href={'#'} className={'text-slate-900 pb-6'}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis
-            iaculis dui porta volutpat. In sit amet posuere magna..
-          </Link>
-          <Link href={'#'} className={'text-pink-800 hover:text-black'}>
-            続きを読む
-          </Link>
-        </div>
-      </article>
+          <div className={'bg-white flex flex-col justify-start p-6'}>
+            <Link
+              href={'#'}
+              className={'text-blue-700 text-sm font-bold uppercase pb-4'}
+            >
+              Technology
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className={
+                'text-3xl text-slate-900 font-bold hover:text-gray-700 pb-4'
+              }
+            >
+              {article.title}
+            </Link>
+            <p className={'text-sm pb-3 text-slate-900'}>
+              Published on {article.createdAt}
+            </p>
+            <Link href={`articles/${article.id}`} className={'text-slate-900 pb-6'}>
+              {article.content}
+            </Link>
+            <Link href={`articles/${article.id}`} className={'text-pink-800 hover:text-black'}>
+              続きを読む
+            </Link>
+          </div>
+        </article>
+      ))}
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <div className="md:flex">
       <section className="w-full md:w-2/3 flex flex-col items-center px-3">
-        <ArticleList />
+        <ArticleList articles={articles} />
         {/* <div className="flex items-center py-8">
           <a
             href="#"
