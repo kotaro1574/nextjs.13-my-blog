cbab553a4d1291125666b2ae7189c9ce243e3357
kotaro1574 <63211616+kotaro1574@users.noreply.github.com>
2023-10-25 14:32:01 +0000

    delete: 不要なコード削除

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 
 const Article = async ({ params }: { params: { id: string } }) => {
   const detailArticle = await getDetailArticle(params.id)
-  console.log(detailArticle)
 
   return (
     <div className="max-w-3xl mx-auto p-5">
