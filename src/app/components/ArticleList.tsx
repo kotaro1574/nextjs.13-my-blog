6e6c5544917c452ea6a017916dab5532575abfb9
kotaro1574 <63211616+kotaro1574@users.noreply.github.com>
2023-10-25 14:08:59 +0000

    update: 詳細データを取得＆表示

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -9,11 +9,11 @@ type Props = {
 const ArticleList = ({ articles }: Props) => {
   return (
     <div>
-      {articles.map((article, index) => (
+      {articles.map((article) => (
         <article key={article.id} className={'flex flex-col shadow my-4'}>
           <Link href={`articles/${article.id}`} className={'hover:opacity-75'}>
             <Image
-              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${index}`}
+              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
               alt={''}
               width={1280}
               height={300}
@@ -37,10 +37,16 @@ const ArticleList = ({ articles }: Props) => {
             <p className={'text-sm pb-3 text-slate-900'}>
               Published on {article.createdAt}
             </p>
-            <Link href={`articles/${article.id}`} className={'text-slate-900 pb-6'}>
+            <Link
+              href={`articles/${article.id}`}
+              className={'text-slate-900 pb-6'}
+            >
               {article.content}
             </Link>
-            <Link href={`articles/${article.id}`} className={'text-pink-800 hover:text-black'}>
+            <Link
+              href={`articles/${article.id}`}
+              className={'text-pink-800 hover:text-black'}
+            >
               続きを読む
             </Link>
           </div>
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,3 +1,4 @@
+import { notFound } from 'next/navigation'
 import { Article } from './types'
 
 export const getAllArticles = async (): Promise<Article[]> => {
@@ -12,4 +13,22 @@ export const getAllArticles = async (): Promise<Article[]> => {
 
   const articles = await res.json()
   return articles
+}
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(
+    `http://0.0.0.0:3001/posts/${id}`,
+    { next: { revalidate: 60 } }, // ISR
+  )
+
+  if (res.status === 404) {
+    notFound()
+  }
+
+  if (!res.ok) {
+    throw new Error('エラー')
+  }
+
+  const article = await res.json()
+  return article
 }
