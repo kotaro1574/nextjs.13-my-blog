fe5eeae33e63df91e3e8a3a0f0ff47eaea6a1d51
kotaro1574 <63211616+kotaro1574@users.noreply.github.com>
2023-11-02 14:23:19 +0000

    update: supabaseから全Data取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import { getAllArticles } from '@/blogAPI'
 import ArticleList from './components/ArticleList'
 
 export default async function Home() {
-  const articles = await getAllArticles()
+  const API_URL = process.env.NEXT_PUBLIC_API_URL!
+
+  const res = await fetch(`${API_URL}/api`, { cache: 'no-store' })
+  const articles = await res.json()
 
   return (
     <div className="md:flex">
