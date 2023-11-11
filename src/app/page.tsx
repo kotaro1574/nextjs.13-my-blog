9ca3cb4c3f130c6d408fc2bc5c64e678f27463b3
kotaro1574 <63211616+kotaro1574@users.noreply.github.com>
2023-11-11 13:07:56 +0000

    update: tweets取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import ArticleList from './components/ArticleList'
+import { cookies } from 'next/headers'
+import AuthButton from './components/AuthButton'
 
 export default async function Home() {
   const API_URL = process.env.NEXT_PUBLIC_API_URL!
@@ -6,57 +9,31 @@ export default async function Home() {
   const res = await fetch(`${API_URL}/api/blog`, { cache: 'no-store' })
   const articles = await res.json()
 
+  const supabase = createServerComponentClient({ cookies })
+  const { data: tweets } = await supabase.from('tweets').select()
+
   return (
     <div className="md:flex">
       <section className="w-full md:w-2/3 flex flex-col items-center px-3">
         <ArticleList articles={articles} />
-        {/* <div className="flex items-center py-8">
-          <a
-            href="#"
-            className="h-10 w-10 bg-blue-800 hover:bg-blue-600 font-semibold text-white text-sm flex items-center justify-center"
-          >
-            1
-          </a>
-          <a
-            href="#"
-            className="h-10 w-10 font-semibold text-gray-800 hover:bg-blue-600 hover:text-white text-sm flex items-center justify-center"
-          >
-            2
-          </a>
-          <a
-            href="#"
-            className="h-10 w-10 font-semibold text-gray-800 hover:text-gray-900 text-sm flex items-center justify-center ml-3"
-          >
-            Next <i className="fas fa-arrow-right ml-2"></i>
-          </a>
-        </div> */}
       </section>
 
       <aside className="w-full md:w-1/3 flex flex-col items-center px-3 md:pl-6">
-        <div className="bg-white shadow-md rounded p-4 mb-6 mt-4">
-          <h3 className="font-bold text-gray-900 mb-2">About Me</h3>
-          <p className="text-gray-600">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            varius enim in eros elementum tristique.
-          </p>
+        <div>
+          <AuthButton />
         </div>
-        <div className="bg-white shadow-md rounded p-4 mb-6 mt-4 w-full">
-          <h3 className="font-bold text-gray-900 mb-2">Category</h3>
-          <ul className="text-gray-600 mt-2">
-            <li>
-              <a href="#">Technology</a>
-            </li>
-            <li>
-              <a href="#">Automotive</a>
-            </li>
-            <li>
-              <a href="#">Finance</a>
-            </li>
-            <li>
-              <a href="#">Sports</a>
-            </li>
-          </ul>
-        </div>
+        {tweets?.map((tweet) => (
+          <div
+            key={tweet.id}
+            className="bg-white shadow-md rounded p-4 mb-6 mt-4 w-full"
+          >
+            <h3 className="font-bold text-gray-900 mb-2">{tweet.title}</h3>
+            {/* <p className="text-gray-600">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse varius enim in eros elementum tristique.
+            </p> */}
+          </div>
+        ))}
       </aside>
     </div>
   )
